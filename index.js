1efa90774b1058d95efe1045cf5a070d9ede6592
mahfuzurmunna <mahfuzurrm07@gmail.com>
2023-06-01 16:29:10 +0600

    dynamic routing

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ const allChefs = require("./data.json")
 
 app.get("/", (req, res) => {
   res.json("hello world");
+
 });
+
+
 
 app.get('/allData', (req,res)=> {
   res.send(allChefs);
 })
+
+app.get("/allData/:id", (req, res) => {
+  const id = req.params.id;
+  const item = allChefs[0]?.chefs.find(pd => pd.id== id)
+  console.log(item);
+  res.send({item})
+});
 
 app.listen(5000, ()=> {
   console.log('server is running on port 5000')
